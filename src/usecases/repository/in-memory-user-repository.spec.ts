70e2cce66af8732494639b6c6b6df48510f81e1f
Igor Magro <iggormagro8@gmail.com>
2020-12-06 23:42:21 -0300

    test: return all users in the repository

--- src/usecases/repository/in-memory-user-repository.spec.ts
+++ src/usecases/repository/in-memory-user-repository.spec.ts
@@ -20,4 +20,15 @@ describe('In memory User repository', () => {
         const user = await userRepo.findUserByEmail(email);
         expect(user.name).toBe(name)
     })
+    
+    test('should return all users in repository', async () => {
+        const users: UserData[] = [
+            {name: 'any_name', email: 'any@mail.com'}, 
+            {name: 'second_name', email: 'second@mail.com'}
+        ]
+
+        const userRepo = new InMemoryUserRepository(users)
+        const returnedUsers = await userRepo.findAllUsers()
+        expect(returnedUsers.length).toBe(2)
+    })
 })
