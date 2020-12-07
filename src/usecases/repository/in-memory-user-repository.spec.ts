b8655eb325e75e400a69692bb07d2fcb66ff9c98
Igor Magro <iggormagro8@gmail.com>
2020-12-06 23:36:26 -0300

    feat: add and find user and check its existance

--- src/usecases/repository/in-memory-user-repository.spec.ts
+++ src/usecases/repository/in-memory-user-repository.spec.ts
@@ -6,7 +6,18 @@ describe('In memory User repository', () => {
     test('should return null if user is not found', async () => {
         const users: UserData[] = []
         const userRepo = new InMemoryUserRepository(users)
-        const user = await userRepo.findUserByEmail('any@email.com');
+        const user = await userRepo.findUserByEmail('any@mail.com');
         expect(user).toBeNull()
+    })
+
+    test('should return user if it is found in te repository', async () => {
+        const users: UserData[] = []
+        const name = 'any_name'
+        const email = 'any@mail.com'
+        const userRepo = new InMemoryUserRepository(users)
+
+        await userRepo.add({name, email})
+        const user = await userRepo.findUserByEmail(email);
+        expect(user.name).toBe(name)
     })
 })
