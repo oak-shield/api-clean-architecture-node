a8bd36f6438479228910e18c72f409559ecb29e6
Igor Magro <iggormagro8@gmail.com>
2020-12-29 20:39:06 -0300

    test: verify e-mail address when creating a user

---
+++ src/entities/user.spec.ts
@@ -0,0 +1,12 @@
+import { InvalidEmailError } from './errors/InvalidEmailError';
+import { left } from './../shared/either';
+import { User } from "./user"
+
+
+describe('User domain class', () => {
+    test('should not create user with invalid e-mail address', () => {
+        const invalidEmail = 'invalid_email'
+        const error = User.create({name: 'anyName', email: 'invalidEemail'})
+        expect(error).toEqual(left(new InvalidEmailError()))
+    })
+})
